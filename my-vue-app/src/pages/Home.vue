1bbac41c3ea60853979abf3827edbf413993f181
seif1125 <seifammar1125@gmail.com>
2023-10-15 15:31:38 +0300

    Add same component with three difrent types

--- my-vue-app/src/pages/Home.vue
+++ my-vue-app/src/pages/Home.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue'
+import MainItem from '../components/MainItem.vue'
 
 defineProps({
   msg: String,
@@ -10,8 +11,47 @@ const count = ref(0)
 
 <template>
   <h1>Homepage message: {{ msg }}</h1>
+  <main-item>
+    <template #icon>
+      <img
+        src="https://static.vecteezy.com/system/resources/previews/000/349/954/original/documentation-vector-icon.jpg"
+        alt="icon heading 1"
+        srcset=""
+        width="50"
+        height="50"
+      />
+    </template>
+    <template #heading><h1>seif heading1</h1></template>
+    <template #body><p>this is paragraph 1</p></template>
+  </main-item>
+  <main-item>
+    <template #icon>
+      <img
+        src="https://static.vecteezy.com/system/resources/previews/000/349/954/original/documentation-vector-icon.jpg"
+        alt="icon heading 1"
+        srcset=""
+        width="50"
+        height="50"
+      />
+    </template>
+    <template #heading><h1>seif heading2</h1></template>
+    <template #body><p>this is paragraph 2</p></template>
+  </main-item>
+  <main-item>
+    <template #icon>
+      <img
+        src="https://static.vecteezy.com/system/resources/previews/000/349/954/original/documentation-vector-icon.jpg"
+        alt="icon heading 1"
+        srcset=""
+        width="50"
+        height="50"
+      />
+    </template>
+    <template #heading><h1>seif heading3</h1></template>
+    <template #body><p>this is paragraph 3</p></template>
+  </main-item>
 </template>
-
+/* we have now the same component with diffrent contents */
 <style scoped>
 .read-the-docs {
   color: #888;
