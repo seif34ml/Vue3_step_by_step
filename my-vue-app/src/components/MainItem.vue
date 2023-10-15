62b533d5975438b3cbd3b3345c467f2be940f334
seif1125 <seifammar1125@gmail.com>
2023-10-15 15:29:28 +0300

    add component with slots

---
+++ my-vue-app/src/components/MainItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <slot name="icon" />
+    <slot name="heading" />
+    <slot name="body" />
+  </div>
+</template>
+
+<script setup>
+name: 'MainItem'
+</script>
+
+<style scoped>
+div {
+  border: 2px solid black;
+  margin-bottom: 15px;
+  padding-top: 15px;
+}
+</style>
